3046c3d1ffa9e0eb073c37dbcd10b299bc3cd729
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-10-25 19:46:06 +0900

    bump up version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gyaodl',
-    version='0.2',
+    version='0.3',
     description='Terminal based GYAO! video downloader.',
     packages=find_packages(),
     entry_points={'console_scripts': ['gyaodl = gyaodl.main:main']}
