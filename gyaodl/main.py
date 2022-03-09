bbdcdf2722c62f24d033e8e1eb6d992bd4474f3e
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-03-09 21:55:23 +0900

    extract parser and other functions to new module

--- gyaodl/main.py
+++ gyaodl/main.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from datetime import datetime, timezone
 import json
 import logging
-from pathlib import Path
 import re
-from html.parser import HTMLParser
-from urllib.parse import quote, unquote, urlencode, urlparse
+from datetime import datetime, timezone
+from http.client import NOT_FOUND
+from pathlib import Path
+from urllib.error import HTTPError
+from urllib.parse import urlencode, urlparse
 from urllib.request import Request, urlopen
-from http.client import HTTPResponse
 
-from gyaodl import dl
-
+from gyaodl import dl, programs
 
 program_version = '0.5'
 
@@ -23,66 +22,6 @@ BRIGHTCOVE_API_PK = (
     'Wv6uhPJHYkaZ6xHINTj5oOqa0-zarOEvQ6e1EqKhBcCppkAU'
     'Wuo5QSKWVC4HZjY2z-Lo_ptwEK3hxfKuvZXkdNuyOM5nNSWy'
 )
-
-
-class VideoIdGetter(HTMLParser):
-    '''
-    VideoIdGetter
-
-    Original parser that parses HTML source of a GYAO! video page and find data-vid attribute.
-    '''
-
-    def __init__(self, *, convert_charrefs=True) -> None:
-        super().__init__(convert_charrefs=convert_charrefs)
-        self.vid = ''
-
-    def handle_starttag(self, _tag: str, attrs: list[tuple]) -> None:
-        # It is guaranteed that the tuple in attrs has only two elements.
-        d = dict(attrs)
-        if 'gyao-player' in d.get('class', ''):
-            self.vid = d.get('data-vid', '')
-
-    def feed(self, data: str) -> str:
-        super().feed(data)
-        return self.vid
-
-
-class EndPointGetter(HTMLParser):
-    '''
-    EndPointGetter
-
-    A parser that parses given HTML source. It searches for the `data-endpoint-url` attribute
-    relating to the video series and returns its value as the feed() function's return value.
-    '''
-
-    def __init__(self, *, convert_charrefs=True) -> None:
-        super().__init__(convert_charrefs=convert_charrefs)
-        self.endpoint_url = ''
-        self.ptn = re.compile(r'^/api/programs/[0-9a-z-]+/videos$')
-
-    def handle_starttag(self, _tag: str, attrs: list[tuple]) -> None:
-        d = dict(attrs)
-        endpoint_url = d.get('data-endpoint-url')
-        if endpoint_url and self.ptn.match(endpoint_url):
-            self.endpoint_url = endpoint_url
-
-    def feed(self, data: str) -> str:
-        super().feed(data)
-        return self.endpoint_url
-
-
-def gyao_url_to_video_info(url: str) -> str:
-    res: HTTPResponse
-    # If url is already encoded, it will be decoded before re-encoding.
-    # If url is not encoded, it will be encoded.
-    with urlopen(quote(unquote(url), safe='/:+')) as res:
-        html_content = res.read()  # bytes
-        if res.headers.get_content_charset():
-            charset = res.headers.get_content_charset()
-        else:
-            charset = 'utf8'
-        parser = VideoIdGetter()
-        return parser.feed(html_content.decode(charset))
 
 
 def get_video_metadata(gyao_videoid: str) -> dict:
@@ -147,56 +86,35 @@ def get_playlist_url(brightcove_id: str) -> str:
         return ''  # Not found
 
 
-def get_available_episodes(url: str) -> list[str]:
-    res: HTTPResponse
-    # If url is already encoded, it will be decoded before re-encoding.
-    # If url is not encoded, it will be encoded.
-    with urlopen(quote(unquote(url), safe='/:+')) as res:
-        html_content = res.read()  # bytes
-        if res.headers.get_content_charset():
-            charset = res.headers.get_content_charset()
-        else:
-            charset = 'utf8'
-        parser = EndPointGetter()
-        endpoint_path = parser.feed(html_content.decode(charset))
-
-    # replace url.path with endpoint_path
-    with urlopen(url.replace(urlparse(url).path, endpoint_path)) as res:
-        assert 'json' in res.headers.get_content_type(), f'Unexpected Content-Type ({res.headers.get_content_type()})'
-
-        json_body = json.loads(res.read())
-        # The loaded JSON must be "dict" type, and its property "videos" must be "list" type.
-        assert type(json_body) == dict and type(json_body.get('videos')) == list, 'Unexpected JSON schema'
-
-        def _filter(v: dict) -> bool:
-            # ensure "shortWebUrl" key exists
-            if type(v) != dict or not v.get('shortWebUrl'):
-                return False
-            # streamingAvailability: the episode is publicly available or not yet
-            elif str(v.get('streamingAvailability', 'available')).lower() != 'available':
-                return False
+def is_available_episode(v: programs.Video) -> bool:
+    # streamingAvailability: the episode is publicly available or not yet
+    if v.streamingAvailability.lower() == 'available':
+        return True
+    else:
+        # The following blocks are meaningless if "streamingAvailabilty" was
+        # determined based on the period from the start date to the end date.
+        try:
+            start_date = datetime.fromisoformat(v.startDate)
+            end_date = datetime.fromisoformat(v.endDate)
+            if start_date.utcoffset():
+                now = datetime.now(timezone(start_date.utcoffset()))
             else:
-                # The following blocks are meaningless if "streamingAvailabilty" was
-                # determined based on the period from the start date to the end date.
-                try:
-                    start_date = datetime.fromisoformat(v.get('startDate', ''))
-                    end_date = datetime.fromisoformat(v.get('endDate', ''))
-                    if start_date.utcoffset():
-                        now = datetime.now(timezone(start_date.utcoffset()))
-                    else:
-                        now = datetime.now()
-                    return now >= start_date and now < end_date
-                except ValueError: # Invalid isoformat string
-                    pass
+                now = datetime.now()
+            return now >= start_date and now < end_date
+        except ValueError: # Invalid isoformat string
+            pass
 
-                # unknown rule
-                return True
-
-        # shorWebUrl: link to a video page (example https://gyao.yahoo.co.jp/episode/1234)
-        return [v.get('shortWebUrl') for v in json_body.get('videos') if _filter(v)]
+        # unknown rule
+        return False
 
 
-def is_expected_url(url: str) -> bool:
+def is_valid_url(url: str) -> bool:
+    '''
+    Validate URL format
+
+    GYAO URL schema1: gyao.yahoo.co.jp/episode/{Japanese title(may be empty)}/{uuid}
+    GYAO URL schema2: gyao.yahoo.co.jp/title/{Japanese title(may be empty)}/{uuid}
+    '''
     if not re.match(r'^https://gyao.yahoo.co.jp/(episode|title)(/[^/]+/|/)[0-9a-z-]+$', url):
         return False
 
@@ -228,34 +146,34 @@ def main() -> None:
     print('Start')
 
     # Throws an error if the given URL does not follow a supported format
-    if not is_expected_url(args.url):
+    if not is_valid_url(args.url):
         raise ValueError('Unexpected URL')
 
     # Prepare a list of video viewing page URLs.
     # By default, it downloads one episode.
-    episodes: list[str] = [args.url]
+    try:
+        episodes: list[programs.Video] = programs.get_episodes(args.url, args.series)
+    except HTTPError as e:
+        if e.code == NOT_FOUND:
+            print('Requested episode or series not found')
+        else:
+            print(e.reason)
+            logger.error(e.reason)
+        return
 
     # When using the --series option, "gyaodl" downloads all episodes.
     # For example, when you give a URL representing the 3rd episode of the series
     # you want to see to "gyaodl" and that series has 12 episodes in total, "gyaodl"
     # downloads all episodes of the series. It means that you can download all of
     # the episodes from the 1st one to the 12th one at once.
-    if args.series:
-        episodes = get_available_episodes(args.url)
 
-    for ep_url in episodes:
+    for ep in episodes:
         try:
-            # GYAO URL schema1: gyao.yahoo.co.jp/episode/{Japanese title(may be empty)}/{uuid}
-            # GYAO URL schema2: gyao.yahoo.co.jp/title/{Japanese title(may be empty)}/{uuid}
-            # Since uuid in URL path is not always same as gyao_videoid, I have to parse HTML source.
-            gyao_videoid = gyao_url_to_video_info(ep_url)
-
-            # Without --series option, the episode may be unavailable
-            # because we skipped check by get_available_episodes().
-            if len(gyao_videoid) == 0:
-                print('This episode is not available yet or vid not found')
+            if not is_available_episode(ep):
+                print(f'{ep.title} [availability: {ep.streamingAvailability}] [{ep.startDate} ~ {ep.endDate}]')
                 continue
 
+            gyao_videoid = ep.id
             logger.debug(f'gyao_videoid: {gyao_videoid}')
 
             # Convert gyao videoid to video id which is managed by brightcove.com
@@ -303,8 +221,14 @@ def main() -> None:
                 # Done
                 print(f'The video has been saved. ({saved_at})')
 
+        except HTTPError as e:
+            if e.code == NOT_FOUND:
+                print('Requested episode or series not found')
+            else:
+                print(e.reason)
+                logger.error(e.reason)
+
         except AssertionError as ae:
             print(ae)
-            continue
 
     print('Done')
---
+++ gyaodl/programs.py
@@ -0,0 +1,129 @@
+import json
+import re
+from dataclasses import asdict, dataclass, fields
+from html.parser import HTMLParser
+from urllib.error import HTTPError
+from urllib.parse import quote, unquote, urlparse
+from urllib.request import urlopen
+
+
+@dataclass
+class Video:
+    '''
+    Video
+
+    Part of resource comes from program API
+    (gyao.yahoo.co.jp/api/programs/<program_id>/videos)
+    '''
+    id: str
+    title: str
+    shortTitle: str
+    streamingAvailability: str
+    startDate: str
+    endDate: str
+    shortWebUrl: str
+
+    def __post_init__(self):
+        me=asdict(self)
+        for f in fields(self):
+            assert type(me[f.name]) == f.type, f'Cannot assign {me[f.name]} to {f.name}({f.type})'
+
+
+class VideoIdGetter(HTMLParser):
+    '''
+    VideoIdGetter
+
+    Original parser that parses HTML source of a GYAO! video page and find data-vid attribute.
+    '''
+
+    def __init__(self, *, convert_charrefs=True) -> None:
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.vid = ''
+
+    def handle_starttag(self, _tag: str, attrs: list[tuple]) -> None:
+        # It is guaranteed that the tuple in attrs has only two elements.
+        d = dict(attrs)
+        if 'gyao-player' in d.get('class', ''):
+            self.vid = d.get('data-vid', '')
+
+    def feed(self, data: str) -> str:
+        super().feed(data)
+        return self.vid
+
+
+class EndPointGetter(HTMLParser):
+    '''
+    EndPointGetter
+
+    A parser that parses given HTML source. It searches for the `data-endpoint-url` attribute
+    relating to the video series and returns its value as the feed() function's return value.
+    '''
+
+    def __init__(self, *, convert_charrefs=True) -> None:
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.endpoint_url = ''
+        self.ptn = re.compile(r'^/api/programs/[0-9a-z-]+/videos$')
+
+    def handle_starttag(self, _tag: str, attrs: list[tuple]) -> None:
+        d = dict(attrs)
+        endpoint_url = d.get('data-endpoint-url')
+        if endpoint_url and self.ptn.match(endpoint_url):
+            self.endpoint_url = endpoint_url
+
+    def feed(self, data: str) -> str:
+        super().feed(data)
+        return self.endpoint_url
+
+
+def get_episodes(url: str, series: bool) -> list[Video]:
+    try:
+        # If url is already encoded, it will be decoded before re-encoding.
+        # If url is not encoded, it will be encoded.
+        with urlopen(quote(unquote(url), safe='/:+')) as res:
+            html_content = res.read()  # bytes
+            if res.headers.get_content_charset():
+                charset = res.headers.get_content_charset()
+            else:
+                charset = 'utf8'
+            endpoint_path = EndPointGetter().feed(html_content.decode(charset))
+            assert len(endpoint_path) > 0, 'Endpoint not found from HTML source'
+
+        # replace url.path with endpoint_path
+        with urlopen(url.replace(urlparse(url).path, endpoint_path)) as res:
+            assert 'json' in res.headers.get_content_type(), f'Unexpected Content-Type ({res.headers.get_content_type()})'
+
+            json_body = json.loads(res.read())
+            # The loaded JSON must be "dict" type, and its property "videos" must be "list" type.
+            assert isinstance(json_body, dict) and isinstance(json_body.get('videos'), list), 'Unexpected JSON schema'
+
+            episodes: list[Video] = []
+            video_fields = [f.name for f in fields(Video)]
+            if series:
+                for v in json_body.get('videos'):
+                    assert isinstance(v, dict), 'Unexpected item found'
+                    # Drop unnecessary properties from response json
+                    filtered_v = dict([p for p in v.items() if p[0] in video_fields])
+                    episodes.append(Video(**filtered_v))
+            else:
+                if url.find('title') > 0:
+                    # Since uuid in URL path is not gyao_videoid when URL includes 'title', so parse HTML
+                    vid = VideoIdGetter().feed(html_content.decode(charset))
+                else:
+                    # Otherwise, the last element of url.path is gyao_videoid
+                    vid = urlparse(url).path.split('/')[-1]
+                assert len(vid) > 0, 'Failed to get videoid'
+
+                for v in json_body.get('videos'):
+                    assert isinstance(v, dict), 'Unexpected item found'
+                    if v.get('id') == vid:
+                        filtered_v = dict([p for p in v.items() if p[0] in video_fields])
+                        episodes.append(Video(**filtered_v))
+                        break
+                    else:
+                        pass
+                # len(episodes) must be 1
+            assert len(episodes) != 0, 'episode list is empty why???'
+            return episodes
+
+    except HTTPError:
+        raise
