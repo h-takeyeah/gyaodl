7c2b9ff02ed8cea25fd722658dea87c07ce6d74c
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-03-10 00:22:37 +0900

    remove useless try except

--- gyaodl/programs.py
+++ gyaodl/programs.py
@@ -78,54 +78,50 @@ class EndPointGetter(HTMLParser):
 
 
 def get_episodes(url: str, series: bool) -> list[Video]:
-    try:
-        # If url is already encoded, it will be decoded before re-encoding.
-        # If url is not encoded, it will be encoded.
-        with urlopen(quote(unquote(url), safe='/:+')) as res:
-            html_content = res.read()  # bytes
-            if res.headers.get_content_charset():
-                charset = res.headers.get_content_charset()
+    # If url is already encoded, it will be decoded before re-encoding.
+    # If url is not encoded, it will be encoded.
+    with urlopen(quote(unquote(url), safe='/:+')) as res:
+        html_content = res.read()  # bytes
+        if res.headers.get_content_charset():
+            charset = res.headers.get_content_charset()
+        else:
+            charset = 'utf8'
+        endpoint_path = EndPointGetter().feed(html_content.decode(charset))
+        assert len(endpoint_path) > 0, 'Endpoint not found from HTML source'
+
+    # replace url.path with endpoint_path
+    with urlopen(url.replace(urlparse(url).path, endpoint_path)) as res:
+        assert 'json' in res.headers.get_content_type(), f'Unexpected Content-Type ({res.headers.get_content_type()})'
+
+        json_body = json.loads(res.read())
+        # The loaded JSON must be "dict" type, and its property "videos" must be "list" type.
+        assert isinstance(json_body, dict) and isinstance(json_body.get('videos'), list), 'Unexpected JSON schema'
+
+        episodes: list[Video] = []
+        video_fields = [f.name for f in fields(Video)]
+        if series:
+            for v in json_body.get('videos'):
+                assert isinstance(v, dict), 'Unexpected item found'
+                # Drop unnecessary properties from response json
+                filtered_v = dict([p for p in v.items() if p[0] in video_fields])
+                episodes.append(Video(**filtered_v))
+        else:
+            if url.find('title') > 0:
+                # Since uuid in URL path is not gyao_videoid when URL includes 'title', so parse HTML
+                vid = VideoIdGetter().feed(html_content.decode(charset))
             else:
-                charset = 'utf8'
-            endpoint_path = EndPointGetter().feed(html_content.decode(charset))
-            assert len(endpoint_path) > 0, 'Endpoint not found from HTML source'
+                # Otherwise, the last element of url.path is gyao_videoid
+                vid = urlparse(url).path.split('/')[-1]
+            assert len(vid) > 0, 'Failed to get videoid'
 
-        # replace url.path with endpoint_path
-        with urlopen(url.replace(urlparse(url).path, endpoint_path)) as res:
-            assert 'json' in res.headers.get_content_type(), f'Unexpected Content-Type ({res.headers.get_content_type()})'
-
-            json_body = json.loads(res.read())
-            # The loaded JSON must be "dict" type, and its property "videos" must be "list" type.
-            assert isinstance(json_body, dict) and isinstance(json_body.get('videos'), list), 'Unexpected JSON schema'
-
-            episodes: list[Video] = []
-            video_fields = [f.name for f in fields(Video)]
-            if series:
-                for v in json_body.get('videos'):
-                    assert isinstance(v, dict), 'Unexpected item found'
-                    # Drop unnecessary properties from response json
+            for v in json_body.get('videos'):
+                assert isinstance(v, dict), 'Unexpected item found'
+                if v.get('id') == vid:
                     filtered_v = dict([p for p in v.items() if p[0] in video_fields])
                     episodes.append(Video(**filtered_v))
-            else:
-                if url.find('title') > 0:
-                    # Since uuid in URL path is not gyao_videoid when URL includes 'title', so parse HTML
-                    vid = VideoIdGetter().feed(html_content.decode(charset))
+                    break
                 else:
-                    # Otherwise, the last element of url.path is gyao_videoid
-                    vid = urlparse(url).path.split('/')[-1]
-                assert len(vid) > 0, 'Failed to get videoid'
-
-                for v in json_body.get('videos'):
-                    assert isinstance(v, dict), 'Unexpected item found'
-                    if v.get('id') == vid:
-                        filtered_v = dict([p for p in v.items() if p[0] in video_fields])
-                        episodes.append(Video(**filtered_v))
-                        break
-                    else:
-                        pass
-                # len(episodes) must be 1
-            assert len(episodes) != 0, 'episode list is empty why???'
-            return episodes
-
-    except HTTPError:
-        raise
+                    pass
+            # len(episodes) must be 1
+        assert len(episodes) != 0, 'episode list is empty why???'
+        return episodes
